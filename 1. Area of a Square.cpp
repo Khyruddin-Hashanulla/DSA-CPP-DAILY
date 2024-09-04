f7eadbd4d94169ca2074a2bbf6447fda0bf50002
Khyruddin Hashanulla <161044712+Khyruddin-Hashanulla@users.noreply.github.com>
2024-09-04 13:16:23 +0530

    push

---
+++ 1. Area of a Square.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter the length of a : ";
+    cin >> a;
+    int area = a * a;
+    cout << "Area of a Square is : " << area << endl;
+}
